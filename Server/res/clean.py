3b720c23ee458ec67f3ed07b6bf97e265a2df1e2
boeing888 <735369269@qq.com>
2017-10-21 13:24:24 +0800

    test

--- Server/res/clean.py
+++ Server/res/clean.py
@@ -5,5 +5,5 @@ import subprocess
 
 subprocess.call('rm -f /conf/session.dat'.split(),shell=False)
 subprocess.call('touch /conf/session.dat'.split(),shell=False)
-subprocess.call('rm -f /data/downloads/*.*'.split(),shell=False)
+subprocess.call('rm -f /data/downloads/*',shell=True)
 print('data cleared')
